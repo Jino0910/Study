5edb11df0cb19d758b46f4c0d34ab2fc68875ff9
gray <gray.b@kakaocorp.com>
2022-09-17 17:49:56 +0900

    [Add]  프로젝트 생성

---
+++ Swift/Shorts/Shorts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shorts
+//
+//  Created by jinho on 2022/09/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift/Shorts/Shorts/ShortsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShortsApp.swift
+//  Shorts
+//
+//  Created by jinho on 2022/09/17.
+//
+
+import SwiftUI
+
+@main
+struct ShortsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
